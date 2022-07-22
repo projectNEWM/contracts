0ba95520b0f7a408e2699c55a6e5d26c272e35c5
logicalmechanism <m.quinn.p@protonmail.com>
2022-07-22 11:06:05 -0700

    Additional testing and improving trace errors

--- nft-locking-contract/src/CheckFuncs.hs
+++ nft-locking-contract/src/CheckFuncs.hs
@@ -29,14 +29,13 @@ module CheckFuncs
   ( isValueContinuing
   , isSingleScript
   ) where
--- import qualified Plutus.V1.Ledger.Address as Address
 import qualified Plutus.V1.Ledger.Value as Value
-import           Ledger                   hiding (singleton)
+import           Ledger                 hiding ( singleton )
 import           PlutusTx.Prelude 
 {- |
   Author   : The Ancient Kraken
   Copyright: 2022
-  Version  : Rev 2
+  Version  : Rev 1
 -}
 -------------------------------------------------------------------------------
 -- | Search each TxOut for a value.
@@ -56,8 +55,8 @@ isSingleScript :: [TxInInfo] -> Bool
 isSingleScript txInputs = loopInputs txInputs 0
   where
     loopInputs :: [TxInInfo] -> Integer -> Bool
-    loopInputs []      counter = counter == 1
-    loopInputs (x:xs) !counter = 
+    loopInputs []     counter = counter == 1
+    loopInputs (x:xs) counter = 
       case txOutDatumHash $ txInInfoResolved x of
-        Nothing -> do counter <= 1 && loopInputs xs counter
-        Just _  -> do counter <= 1 && loopInputs xs (counter + 1)
+        Nothing -> loopInputs xs counter
+        Just _  -> loopInputs xs (counter + 1)
