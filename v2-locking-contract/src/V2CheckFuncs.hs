f605ea9c58f02a3b0128af0d3b63649702958ac3
logicalmechanism <m.quinn.p@protonmail.com>
2022-07-26 13:58:32 -0700

    v2 fractional locking contract upgraded

--- v2-locking-contract/src/V2CheckFuncs.hs
+++ v2-locking-contract/src/V2CheckFuncs.hs
@@ -25,7 +25,7 @@
 {-# OPTIONS_GHC -fobject-code                 #-}
 {-# OPTIONS_GHC -fno-specialise               #-}
 {-# OPTIONS_GHC -fexpose-all-unfoldings       #-}
-module CheckFuncs
+module V2CheckFuncs
   ( isValueContinuing
   , isAddrGettingPaid
   , isSingleScript
@@ -33,52 +33,53 @@ module CheckFuncs
   ) where
 import           Plutus.V1.Ledger.Credential
 import qualified Plutus.V1.Ledger.Value      as Value
-import           Ledger                      hiding ( singleton )
+import qualified Plutus.V2.Ledger.Api        as PlutusV2
 import           PlutusTx.Prelude 
 {- |
   Author   : The Ancient Kraken
   Copyright: 2022
-  Version  : Rev 1
+  Version  : Rev 2
 -}
 -------------------------------------------------------------------------
 -- | Create a proper Address Type.
 -------------------------------------------------------------------------
-createAddress :: PubKeyHash -> PubKeyHash -> Address
-createAddress pkh sc = if getPubKeyHash sc == emptyByteString then Address (PubKeyCredential pkh) Nothing else Address (PubKeyCredential pkh) (Just $ StakingHash $ PubKeyCredential sc)
+createAddress :: PlutusV2.PubKeyHash -> PlutusV2.PubKeyHash -> PlutusV2.Address
+createAddress pkh sc = if PlutusV2.getPubKeyHash sc == emptyByteString then PlutusV2.Address (PubKeyCredential pkh) Nothing else PlutusV2.Address (PubKeyCredential pkh) (Just $ StakingHash $ PubKeyCredential sc)
 -------------------------------------------------------------------------------
 -- | Search each TxOut for a value.
 -------------------------------------------------------------------------------
-isValueContinuing :: [TxOut] -> Value -> Bool
+isValueContinuing :: [PlutusV2.TxOut] -> PlutusV2.Value -> Bool
 isValueContinuing [] _ = False
 isValueContinuing (x:xs) val
   | checkVal  = True
   | otherwise = isValueContinuing xs val
   where
     checkVal :: Bool
-    checkVal = Value.geq (txOutValue x) val
+    checkVal = Value.geq (PlutusV2.txOutValue x) val
 -------------------------------------------------------------------------------
 -- | Search each TxOut for an pkh and value.
 -------------------------------------------------------------------------------
-isAddrGettingPaid :: [TxOut] -> Address -> Value -> Bool
-isAddrGettingPaid [] _addr _val = False
+isAddrGettingPaid :: [PlutusV2.TxOut] -> PlutusV2.Address -> PlutusV2.Value -> Bool
+isAddrGettingPaid []     _    _ = False
 isAddrGettingPaid (x:xs) addr val
   | checkAddr && checkVal = True
   | otherwise             = isAddrGettingPaid xs addr val
   where
     checkAddr :: Bool
-    checkAddr = txOutAddress x == addr
+    checkAddr = PlutusV2.txOutAddress x == addr
 
     checkVal :: Bool
-    checkVal = Value.geq (txOutValue x) val
+    checkVal = Value.geq (PlutusV2.txOutValue x) val
 -------------------------------------------------------------------------------
 -- | Force a single script utxo input.
 -------------------------------------------------------------------------------
-isSingleScript :: [TxInInfo] -> Bool
+isSingleScript :: [PlutusV2.TxInInfo] -> Bool
 isSingleScript txInputs = loopInputs txInputs 0
   where
-    loopInputs :: [TxInInfo] -> Integer -> Bool
-    loopInputs []     counter = counter == (1 :: Integer)
+    loopInputs :: [PlutusV2.TxInInfo] -> Integer -> Bool
+    loopInputs []     counter = counter == 1
     loopInputs (x:xs) counter = 
-      case txOutDatumHash $ txInInfoResolved x of
-        Nothing -> loopInputs xs counter
-        Just _  -> loopInputs xs (counter + 1)
+      case PlutusV2.txOutDatum $ PlutusV2.txInInfoResolved x of
+        PlutusV2.NoOutputDatum       -> loopInputs xs counter
+        (PlutusV2.OutputDatumHash _) -> loopInputs xs (counter + 1)
+        (PlutusV2.OutputDatum     _) -> loopInputs xs (counter + 1)
--- v2-locking-contract/src/V2LockingContract.hs
+++ v2-locking-contract/src/V2LockingContract.hs
@@ -25,53 +25,46 @@
 {-# OPTIONS_GHC -fobject-code                 #-}
 {-# OPTIONS_GHC -fno-specialise               #-}
 {-# OPTIONS_GHC -fexpose-all-unfoldings       #-}
-module LockingContract
+module V2LockingContract
   ( lockingContractScript
   , lockingContractScriptShortBs
   , CustomDatumType
-  , Schema
-  , contract
   ) where
 import qualified PlutusTx
 import           PlutusTx.Prelude
-import           Plutus.Contract
-import           Cardano.Api.Shelley      ( PlutusScript (..), PlutusScriptV1 )
-import           Codec.Serialise          ( serialise )
-import qualified Data.ByteString.Lazy     as LBS
-import qualified Data.ByteString.Short    as SBS
-import           Ledger                   hiding ( singleton )
-import qualified Ledger.Typed.Scripts     as Scripts
-import qualified Plutus.V1.Ledger.Scripts as Plutus
-import qualified Plutus.V1.Ledger.Value   as Value
-import           Data.Aeson               ( FromJSON, ToJSON )
-import           Data.OpenApi.Schema      ( ToSchema )
-import           GHC.Generics             ( Generic )
-import           Prelude                  ( Show )
-import           CheckFuncs
+import           Cardano.Api.Shelley            ( PlutusScript (..), PlutusScriptV2 )
+import           Codec.Serialise                ( serialise )
+import qualified Data.ByteString.Lazy           as LBS
+import qualified Data.ByteString.Short          as SBS
+import qualified Plutus.V1.Ledger.Scripts       as Scripts
+import qualified Plutus.V1.Ledger.Value         as Value
+import qualified Plutus.V2.Ledger.Contexts      as ContextsV2
+import qualified Plutus.V2.Ledger.Api           as PlutusV2
+import           Plutus.Script.Utils.V2.Scripts as Utils
+import           V2CheckFuncs
 {- |
   Author   : The Ancient Kraken
   Copyright: 2022
-  Version  : Rev 1
+  Version  : Rev 2
 -}
 -------------------------------------------------------------------------------
 -- | Create the datum parameters data object.
 -------------------------------------------------------------------------------
 data CustomDatumType = CustomDatumType
-    { cdtFractionalPid :: !CurrencySymbol
+    { cdtFractionalPid :: PlutusV2.CurrencySymbol
     -- ^ The Newm fractionalization minting policy
-    , cdtTokenizedPid  :: !CurrencySymbol
+    , cdtTokenizedPid  :: PlutusV2.CurrencySymbol
     -- ^ The artist's tokenized policy id
-    , cdtTokenizedTn   :: !TokenName
+    , cdtTokenizedTn   :: PlutusV2.TokenName
     -- ^ the artist's tokenized token name.
-    , cdtArtistPKH     :: !PubKeyHash
+    , cdtArtistPKH     :: PlutusV2.PubKeyHash
     -- ^ The artist's public key hash.
-    , cdtArtistSC      :: !PubKeyHash
+    , cdtArtistSC      :: PlutusV2.PubKeyHash
     -- ^ The artist's staking key hash.
+    , cdtNewmPKH       :: PlutusV2.PubKeyHash
+    -- ^ The newm's public key hash.
     }
-    deriving stock (Show, Generic)
-    deriving anyclass (FromJSON, ToJSON, ToSchema)
 PlutusTx.unstableMakeIsData ''CustomDatumType
-PlutusTx.makeLift ''CustomDatumType
 -- old == new
 instance Eq CustomDatumType where
   {-# INLINABLE (==) #-}
@@ -79,12 +72,8 @@ instance Eq CustomDatumType where
            ( cdtTokenizedPid  a == cdtTokenizedPid  b ) &&
            ( cdtTokenizedTn   a == cdtTokenizedTn   b ) &&
            ( cdtArtistPKH     a == cdtArtistPKH     b ) &&
-           ( cdtArtistSC      a == cdtArtistSC      b )
--------------------------------------------------------------------------------
--- | Create the contract parameters data object.
--------------------------------------------------------------------------------
-newtype LockingContractParams = LockingContractParams { lcNewmPKH :: PubKeyHash }
-PlutusTx.makeLift ''LockingContractParams
+           ( cdtArtistSC      a == cdtArtistSC      b ) &&
+           ( cdtNewmPKH       a == cdtNewmPKH       b )
 -------------------------------------------------------------------------------
 -- | Create the redeemer type.
 -------------------------------------------------------------------------------
@@ -95,16 +84,15 @@ PlutusTx.makeIsDataIndexed ''CustomRedeemerType [ ('Lock,   0)
                                                 , ('Unlock, 1)
                                                 , ('Exit,   2)
                                                 ]
-PlutusTx.makeLift ''CustomRedeemerType
 -------------------------------------------------------------------------------
--- | mkValidator :: TypeData -> Datum -> Redeemer -> ScriptContext -> Bool
+-- | mkValidator :: Datum -> Redeemer -> ScriptContext -> Bool
 -------------------------------------------------------------------------------
 {-# INLINABLE mkValidator #-}
-mkValidator :: LockingContractParams -> CustomDatumType -> CustomRedeemerType -> ScriptContext -> Bool
-mkValidator lcp datum redeemer context =
+mkValidator :: CustomDatumType -> CustomRedeemerType -> PlutusV2.ScriptContext -> Bool
+mkValidator datum redeemer context =
   case redeemer of
     Lock -> do 
-      { let a = traceIfFalse "Signing Tx Error"    $ txSignedBy info newmPKH && txSignedBy info artistPKH
+      { let a = traceIfFalse "Signing Tx Error"    $ ContextsV2.txSignedBy info newmPKH && ContextsV2.txSignedBy info artistPKH
       ; let b = traceIfFalse "Single Script Error" $ isSingleScript txInputs
       ; let c = traceIfFalse "Cont Payin Error"    $ isValueContinuing contOutputs (validatingValue + singularNFT)
       ; let d = traceIfFalse "FT Mint Error"       checkMintedAmount
@@ -112,7 +100,7 @@ mkValidator lcp datum redeemer context =
       ;         traceIfFalse "Lock Endpoint Error" $ all (==True) [a,b,c,d,e]
       }
     Unlock -> do 
-      { let a = traceIfFalse "Signing Tx Error"      $ txSignedBy info newmPKH && txSignedBy info artistPKH
+      { let a = traceIfFalse "Signing Tx Error"      $ ContextsV2.txSignedBy info newmPKH && ContextsV2.txSignedBy info artistPKH
       ; let b = traceIfFalse "Single Script Error"   $ isSingleScript txInputs
       ; let c = traceIfFalse "NFT Payout Error"      $ isAddrGettingPaid txOutputs artistAddr singularNFT
       ; let d = traceIfFalse "Cont Payin Error"      $ isValueContinuing contOutputs (validatingValue - singularNFT)
@@ -121,102 +109,90 @@ mkValidator lcp datum redeemer context =
       ;         traceIfFalse "Unlock Endpoint Error" $ all (==True) [a,b,c,d,e,f]
       }
     Exit -> do 
-      { let a = traceIfFalse "Signing Tx Error"    $ txSignedBy info newmPKH
+      { let a = traceIfFalse "Signing Tx Error"    $ ContextsV2.txSignedBy info newmPKH
       ;         traceIfFalse "Exit Endpoint Error" $ all (==True) [a]
       }
    where
-    info :: TxInfo
-    info = scriptContextTxInfo  context
+    info :: PlutusV2.TxInfo
+    info = PlutusV2.scriptContextTxInfo context
 
     -- inputs / outputs
-    contOutputs :: [TxOut]
-    contOutputs = getContinuingOutputs context
+    contOutputs :: [PlutusV2.TxOut]
+    contOutputs = ContextsV2.getContinuingOutputs context
 
-    txInputs :: [TxInInfo]
-    txInputs = txInfoInputs  info
+    txInputs :: [PlutusV2.TxInInfo]
+    txInputs = PlutusV2.txInfoInputs  info
 
-    txOutputs :: [TxOut]
-    txOutputs = txInfoOutputs info
+    txOutputs :: [PlutusV2.TxOut]
+    txOutputs = PlutusV2.txInfoOutputs info
 
     -- newm info
-    newmPKH :: PubKeyHash
-    newmPKH = lcNewmPKH lcp
+    newmPKH :: PlutusV2.PubKeyHash
+    newmPKH = cdtNewmPKH datum
 
     -- artist info
-    artistPKH :: PubKeyHash
+    artistPKH :: PlutusV2.PubKeyHash
     artistPKH = cdtArtistPKH datum
 
-    artistSC :: PubKeyHash
+    artistSC :: PlutusV2.PubKeyHash
     artistSC = cdtArtistSC datum
 
-    artistAddr :: Address
+    artistAddr :: PlutusV2.Address
     artistAddr =  createAddress artistPKH artistSC
 
-    -- value info
-    validatingValue :: Value
-    validatingValue = 
-      case findOwnInput context of
+    -- token info
+    validatingValue :: PlutusV2.Value
+    validatingValue =
+      case ContextsV2.findOwnInput context of
         Nothing    -> traceError "No Input to Validate." -- This error should never be hit.
-        Just input -> txOutValue $ txInInfoResolved input
+        Just input -> PlutusV2.txOutValue $ PlutusV2.txInInfoResolved input
 
-    singularNFT :: Value
+    singularNFT :: PlutusV2.Value
     singularNFT = Value.singleton (cdtTokenizedPid datum) (cdtTokenizedTn datum) (1 :: Integer)
 
     -- minting
     checkMintedAmount :: Bool
     checkMintedAmount = 
-      case Value.flattenValue (txInfoMint info) of
+      case Value.flattenValue (PlutusV2.txInfoMint info) of
         [(cs, tn, _)] -> cs == cdtFractionalPid datum && tn == cdtTokenizedTn datum
         _             -> False
     
-    isEmbeddedDatum :: [TxOut] -> Bool
+    -- check if the incoming datum is the correct form.
+    isEmbeddedDatum :: [PlutusV2.TxOut] -> Bool
     isEmbeddedDatum []     = False
     isEmbeddedDatum (x:xs) = 
-      case txOutDatumHash x of
-        Nothing -> isEmbeddedDatum xs
-        Just dh -> 
-          case findDatum dh info of
-            Nothing        -> False
-            Just (Datum d) -> 
-              case PlutusTx.fromBuiltinData d of
-                Nothing       -> False
+      case PlutusV2.txOutDatum x of
+        -- datumless
+        PlutusV2.NoOutputDatum -> isEmbeddedDatum xs
+        -- inline datum
+        (PlutusV2.OutputDatum (PlutusV2.Datum d)) -> 
+          case PlutusTx.fromBuiltinData d of
+            Nothing     -> isEmbeddedDatum xs
+            Just inline -> datum == inline
+        -- embedded datum
+        (PlutusV2.OutputDatumHash dh)             -> 
+          case ContextsV2.findDatum dh info of
+            Nothing                  -> isEmbeddedDatum xs
+            Just (PlutusV2.Datum d') -> 
+              case PlutusTx.fromBuiltinData d' of
+                Nothing       -> isEmbeddedDatum xs
                 Just embedded -> datum == embedded
 -------------------------------------------------------------------------------
--- | This determines the data type for Datum and Redeemer.
+-- | Now we need to compile the Validator.
 -------------------------------------------------------------------------------
-data Typed
-instance Scripts.ValidatorTypes Typed where
-  type instance DatumType    Typed = CustomDatumType
-  type instance RedeemerType Typed = CustomRedeemerType
--------------------------------------------------------------------------------
--- | Now we need to compile the Typed Validator.
--------------------------------------------------------------------------------
-typedValidator :: LockingContractParams -> Scripts.TypedValidator Typed
-typedValidator rpp = Scripts.mkTypedValidator @Typed
-  ($$(PlutusTx.compile [|| mkValidator ||]) `PlutusTx.applyCode` PlutusTx.liftCode rpp)
-   $$(PlutusTx.compile [|| wrap        ||])
-    where
-      wrap = Scripts.wrapValidator @CustomDatumType @CustomRedeemerType  -- @Datum @Redeemer
--------------------------------------------------------------------------------
--- | Define The Validator Here
--------------------------------------------------------------------------------
-validator :: Plutus.Validator
-validator = Scripts.validatorScript (typedValidator $ LockingContractParams { lcNewmPKH = "a2108b7b1704f9fe12c906096ea1634df8e089c9ccfd651abae4a439" })
+validator' :: PlutusV2.Validator
+validator' = PlutusV2.mkValidatorScript
+    $$(PlutusTx.compile [|| wrap ||])
+ where
+    wrap = Utils.mkUntypedValidator mkValidator
 -------------------------------------------------------------------------------
 -- | The code below is required for the plutus script compile.
 -------------------------------------------------------------------------------
-script :: Plutus.Script
-script = Plutus.unValidatorScript validator
+script :: Scripts.Script
+script = Scripts.unValidatorScript validator'
 
 lockingContractScriptShortBs :: SBS.ShortByteString
 lockingContractScriptShortBs = SBS.toShort . LBS.toStrict $ serialise script
 
-lockingContractScript :: PlutusScript PlutusScriptV1
+lockingContractScript :: PlutusScript PlutusScriptV2
 lockingContractScript = PlutusScriptSerialised lockingContractScriptShortBs
--------------------------------------------------------------------------------
--- | Off Chain
--------------------------------------------------------------------------------
-type Schema = Endpoint "" ()
-
-contract :: AsContractError e => Contract () Schema e ()
-contract = selectList [] >> contract
