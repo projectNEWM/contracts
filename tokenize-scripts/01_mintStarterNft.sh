dde6f72180a48bc35ef80d5bb868eb51b80644f2
Andrew Westberg <andrewwestberg@gmail.com>
2022-08-26 16:55:09 +0000

    improve minutxo and fees for first scripts

--- tokenize-scripts/01_mintStarterNft.sh
+++ tokenize-scripts/01_mintStarterNft.sh
@@ -15,7 +15,9 @@ script_address=$(${cli} address build --payment-script-file ${script_path} --tes
 seller_address=$(cat wallets/seller-wallet/payment.addr)
 multisig_address=$(cat wallets/multisig-wallet/payment.addr)
 seller_pkh=$(${cli} address key-hash --payment-verification-key-file wallets/seller-wallet/payment.vkey)
-collat_pkh=$(${cli} address key-hash --payment-verification-key-file wallets/collat-wallet/payment.vkey)
+multisig1_pkh=$(${cli} address key-hash --payment-verification-key-file wallets/multisig-wallet/multisig1.vkey)
+multisig2_pkh=$(${cli} address key-hash --payment-verification-key-file wallets/multisig-wallet/multisig2.vkey)
+multisig3_pkh=$(${cli} address key-hash --payment-verification-key-file wallets/multisig-wallet/multisig3.vkey)
 
 policy_id=$(cat policy/starter.id)
 # It'sTheStarterToken4ProjectNewM
@@ -28,11 +30,21 @@ UTXO_VALUE=$(${cli} transaction calculate-min-required-utxo \
     --tx-out="${script_address} ${MINT_ASSET}" | tr -dc '0-9')
 
 script_address_out="${script_address} + 5000000 + ${MINT_ASSET}"
+
+starter_nft_min_utxo=$(${cli} transaction calculate-min-required-utxo \
+    --babbage-era \
+    --protocol-params-file tmp/protocol.json \
+    --tx-out="${script_address_out}" \
+    --tx-out-inline-datum-file data/current_datum.json | tr -dc '0-9')
+    
+echo "Starter NFT Min Fee: "${starter_nft_min_utxo}
+
+script_address_out="${script_address} + $starter_nft_min_utxo + ${MINT_ASSET}"
 echo "Mint OUTPUT: "${script_address_out}
 #
 # exit
 #
-echo -e "\033[0;36m Gathering Buyer UTxO Information  \033[0m"
+echo -e "\033[0;36m Gathering Seller UTxO Information  \033[0m"
 ${cli} query utxo \
     --testnet-magic ${testnet_magic} \
     --address ${seller_address} \
@@ -45,7 +57,6 @@ if [ "${TXNS}" -eq "0" ]; then
 fi
 alltxin=""
 TXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in"' tmp/seller_utxo.json)
-CTXIN=$(jq -r --arg alltxin "" 'keys[] | . + $alltxin + " --tx-in-collateral"' tmp/seller_utxo.json)
 seller_tx_in=${TXIN::-8}
 
 # exit
@@ -59,7 +70,9 @@ FEE=$(${cli} transaction build \
     --tx-out="${script_address_out}" \
     --tx-out-inline-datum-file data/current_datum.json  \
     --required-signer-hash ${seller_pkh} \
-    --required-signer-hash ${collat_pkh} \
+    --required-signer-hash ${multisig1_pkh} \
+    --required-signer-hash ${multisig1_pkh} \
+    --required-signer-hash ${multisig1_pkh} \
     --mint-script-file policy/policy.script \
     --mint="${MINT_ASSET}" \
     --testnet-magic ${testnet_magic})
@@ -74,7 +87,9 @@ echo -e "\033[1;32m Fee: \033[0m" $FEE
 echo -e "\033[0;36m Signing \033[0m"
 ${cli} transaction sign \
     --signing-key-file wallets/seller-wallet/payment.skey \
-    --signing-key-file wallets/collat-wallet/payment.skey \
+    --signing-key-file wallets/multisig-wallet/multisig1.skey \
+    --signing-key-file wallets/multisig-wallet/multisig2.skey \
+    --signing-key-file wallets/multisig-wallet/multisig3.skey \
     --tx-body-file tmp/tx.draft \
     --out-file tmp/tx.signed \
     --testnet-magic ${testnet_magic}
