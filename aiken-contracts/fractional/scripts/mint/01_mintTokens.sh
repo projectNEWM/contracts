ac72bd6afa7ef42fb14522c04e67dc326c72afee
logicalmechanism <logical.mechanism@protonmail.com>
2023-06-19 15:10:36 -0700

    all prefixes are correct

--- aiken-contracts/fractional/scripts/mint/01_mintTokens.sh
+++ aiken-contracts/fractional/scripts/mint/01_mintTokens.sh
@@ -56,9 +56,6 @@ IFS='#' read -ra array <<< "$string"
 prefix_100="000643b0"
 prefix_444="001bc280"
 
-# for testing
-prefix_bad="283329"
-
 ref_name=$(python3 -c "import sys; sys.path.append('../../lib/py/'); from getTokenName import token_name; token_name('${array[0]}', ${array[1]}, '${prefix_100}')")
 frac_name=$(python3 -c "import sys; sys.path.append('../../lib/py/'); from getTokenName import token_name; token_name('${array[0]}', ${array[1]}, '${prefix_444}')")
 
